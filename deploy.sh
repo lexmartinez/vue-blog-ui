3549984c1d057bd523b97cebde90addefee971f7
Lex Martinez <lex0316@gmail.com>
2017-11-17 15:24:53 -0500

    deploy script added

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-cd /dist
-ls
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git init
+  git add -A
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@${GH_REPO_TARGET} > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+cd dist
+setup_git
+commit_website_files
+upload_files
