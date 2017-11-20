895d9f820d198bdd5cbac046f73898c459c1de2f
Lex Martinez <lex0316@gmail.com>
2017-11-20 16:26:08 -0500

    favicon - testing redeploy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Pace from 'pace-progress'
+
+Pace.start()
+Pace.once('hide', () => {
+  window.prerenderReady = true
+})
+
+console.info('%c', 'line-height:48px;padding:18px 150px;background:url(\'https://nerdist.com/wp-content/uploads/2017/04/deadpool-2-featured-image-04222017-322x268.jpg\') center / contain no-repeat;')
 
 Vue.config.productionTip = false
 
