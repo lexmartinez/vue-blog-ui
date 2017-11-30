52d0517bbab9377087c992ae27249aa2d9250e6b
Travis CI <lex0316@gmail.com>
2017-11-30 13:59:20 -0500

    pushing build on deploy branch

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,15 @@ setup_git() {
 }
 
 commit_website_files() {
-  mv ./static/.htaccess ./
-  touch .nojekyll
+  touch .htaccess
+  echo $HTACCESS >.htaccess
   git init
   git add -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  git checkout -b deploy master
   git remote add origin https://${GH_TOKEN}@${GH_REPO_TARGET} > /dev/null 2>&1
   git push -f --quiet --set-upstream origin deploy
 }
