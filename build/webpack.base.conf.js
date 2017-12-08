b6b5fe9047f361354ec8baa8c3f0e36ed16dbe8d
Lex Martinez <lex0316@gmail.com>
2017-12-07 23:14:05 -0500

    adding vue-prism to webpack config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,7 +75,7 @@ module.exports = {
       },{
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-particles')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-particles'), resolve('node_modules/vue-prism')]
       }
     ]
   }
