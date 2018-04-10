800309ba81780221ea483119f4e93fef8d7ffd64
Lex Martinez <lex0316@gmail.com>
2018-04-10 18:54:55 -0500

    including mail option on about screen

--- src/components/BlogNav.vue
+++ src/components/BlogNav.vue
@@ -47,7 +47,8 @@
           <br/>Author this blog. Guitar enthusiast 🤘
         </p>
         <p style="text-align: center">Find me on &nbsp;<a class="fa fa-github-alt" href="https://github.com/lexmartinez" target="_blank"></a> &nbsp;
-          <a class="fa fa-twitter" href="https://twitter.com/lex0316" target="_blank"></a>
+          <a class="fa fa-twitter" href="https://twitter.com/lex0316" target="_blank"></a> &nbsp;
+          <a class="fa fa-envelope" href="mailto:me@lexmartinez.com" target="_blank"></a>
           , DMs are open!</p>
 
       </div>
